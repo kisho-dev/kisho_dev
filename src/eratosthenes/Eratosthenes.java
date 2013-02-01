20e2af1287344fda515e7864893af96a816986eb
kiuchishou <kiuchishou@172.20.10.5>
2013-02-01 23:35:57 +0900

    クラス・パッケージ名変更

--- src/eratosthenes/Eratosthenes.java
+++ src/eratosthenes/Eratosthenes.java
@@ -4,11 +4,12 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.TreeSet;
 
-public class Main_eratosthenes {
+public class Eratosthenes {
 	public static final String EXCEPTION_MESSAGE = "数字を指定してください。";
 
 	public static void main(String[] args) {
 		Integer num = 0;
+		// 引数のチェック
 		try {
 			num = Integer.parseInt(args[0]);
 		} catch (NumberFormatException e) {
@@ -18,32 +19,33 @@ public class Main_eratosthenes {
 		}
 
 		// 指定された数を配列に格納
-		LinkedList<Integer> numList = new LinkedList<Integer>();
+		LinkedList<Integer> setNumList = new LinkedList<Integer>();
 		for (int i = 0; i <= num; i++) {
 			if (i != 0 && i != 1)
-				numList.add(i);
+				setNumList.add(i);
 		}
 
 		// 素数を格納する配列
 		LinkedList<Integer> primeList = new LinkedList<Integer>();
-		// 先頭の数を格納し、その数の倍数をリストから削除
+		
 		do {
-			primeList.add(numList.getFirst());
-			for (Iterator<Integer> iterator = numList.iterator(); iterator
+			// 先頭の数を格納し、その数の倍数をリストから削除
+			primeList.add(setNumList.getFirst());
+			for (Iterator<Integer> iterator = setNumList.iterator(); iterator
 					.hasNext();) {
 				Integer judgeNum = (Integer) iterator.next();
 				if (judgeNum % primeList.getLast() == 0) {
 					iterator.remove();
 				}
 			}
-		} while (primeList.getLast() * primeList.getLast() < numList.getLast());
+		} while (primeList.getLast() * primeList.getLast() < setNumList.getLast());
 
 		//２つの配列を結合し、ソートした配列を作成
 		TreeSet<Integer> compPrimeList = new TreeSet<Integer>();
 		for (Integer integer : primeList) {
 			compPrimeList.add(integer);
 		}
-		for (Integer integer : numList) {
+		for (Integer integer : setNumList) {
 			compPrimeList.add(integer);
 		}
 		System.out.println(compPrimeList);
